ccb0728e0400cc7aed06f675298260fb6e62a2d6
VARUN JAIN <varun13@gmail.com>
2015-10-29 01:28:46 +0530

    added tmc website recipe

---
+++ clarifi_apps/attributes/default.rb
@@ -0,0 +1,6 @@
+default['tmc_website']['site_name'] = 'tmc_website'
+default['tmc_website']['host_header'] = 'tmc.clarifi.io'
+default['tmc_website']['port'] = 80
+default['tmc_website']['protocol'] = :http
+default['tmc_website']['runtime_version'] = '4.0'
+default['tmc_website']['site_base_directory'] = "#{ENV['SYSTEMDRIVE']}\\inetpub\\wwwroot"
--- clarifi_apps/recipes/tmc_website.rb
+++ clarifi_apps/recipes/tmc_website.rb
@@ -3,3 +3,26 @@
 # Recipe:: tmc_website
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+
+Chef::Log.level = :debug
+
+apps = search(:aws_opsworks_app, "deploy:true") rescue []
+app = apps.find {|x| x[:shortname] == "tmc_website"}
+if app
+	Chef::Log.debug "Found #{app[:shortname]} to deploy on the stack. Assuming tmc_website app is same."
+
+	tavisca_apps_website node['tmc_website']['site_name'] do
+	  host_header node['tmc_website']['host_header']
+	  port node['tmc_website']['port']
+	  protocol node['tmc_website']['protocol']
+	  website_base_directory node['tmc_website']['site_base_directory']
+	  runtime_version node['tmc_website']['runtime_version']
+	  scm app["app_source"]
+	  should_replace_web_config false
+	  #web_erb_config 'Web.config.erb'
+	  #web_config_params node['tmc_website']["web_config_params"]
+	  action :add
+	end
+else
+	Chef::Log.debug "tmc_website app not found in apps to deploy."
+end
