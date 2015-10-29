ef2f734fb0b7e67e9db93720508e2ef39100588e
VARUN JAIN <varun13@gmail.com>
2015-10-29 11:56:50 +0530

    commented website provider for testing

--- win_apps/resources/win_service.rb
+++ win_apps/resources/win_service.rb
@@ -1,13 +1,13 @@
-actions :install, :start
-default_action :install
+# actions :install, :start
+# default_action :install
 
-attribute :service_name, :name_attribute => true, :kind_of => String, :required => true
-attribute :service_source, :kind_of => String, :required => true
-attribute :service_executable_with_args, :kind_of => String, :required => true
-attribute :service_install_base_path, :kind_of => String, :default => "#{ENV['SYSTEMDRIVE']}\\winservices"
-attribute :service_start, :kind_of => Symbol, :equal_to => [:automatic, :disabled, :manual],  :default => :manual
-attribute :scm, :kind_of => Hash, :required => true
-attribute :new_app_config, :kind_of => String
-attribute :should_replace_app_config, :kind_of => [TrueClass, FalseClass], :default => false
-attribute :app_erb_config, :kind_of => String
-attribute :app_config_params, :kind_of => Hash
+# attribute :service_name, :name_attribute => true, :kind_of => String, :required => true
+# attribute :service_source, :kind_of => String, :required => true
+# attribute :service_executable_with_args, :kind_of => String, :required => true
+# attribute :service_install_base_path, :kind_of => String, :default => "#{ENV['SYSTEMDRIVE']}\\winservices"
+# attribute :service_start, :kind_of => Symbol, :equal_to => [:automatic, :disabled, :manual],  :default => :manual
+# attribute :scm, :kind_of => Hash, :required => true
+# attribute :new_app_config, :kind_of => String
+# attribute :should_replace_app_config, :kind_of => [TrueClass, FalseClass], :default => false
+# attribute :app_erb_config, :kind_of => String
+# attribute :app_config_params, :kind_of => Hash
